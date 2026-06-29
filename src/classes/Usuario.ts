e7b849d6afc08933e34ba80a8e28031f71829b3f
Bruno Mendes <bruno_b_mendes@estudante.sesisenai.org.br>
2026-06-29 19:51:49 -0300

    classe Usuario finalizado

--- src/classes/Usuario.ts
+++ src/classes/Usuario.ts
@@ -2,7 +2,36 @@ export class Usuario {
     id: Number;
     nome: string;
     email: string;
-    senha: string;
     endereco: string;
+    senha: string;
+        
+    constructor (
+        id:number,
+        nome:string,
+        email:string,
+        endereco:string,
+        senha:string
+    ) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.endereco = endereco;
+        this.senha = senha;
+    }
+    validar(): string | null {
+        if(!this.nome || this.nome.trim().length === 0){
+            return "O Nome é obrigatório."
+        }
+        if(!this.email || this.email.trim().length === 0) {
+            return "O E-mail é obrigatório."
+        }
+        if(!this.endereco || this.endereco.trim().length === 0) {
+            return "O Endereço é obrigatório."
+        }
+        if(!this.senha || this.senha.trim().length === 0) {
+            return "A Senha é obrigatório."
+        }
+        return null;
+    }
     
 }
