f8c5fcbbec2a453dcd7acd0befcd65173a2c75d4
Eduardo Marcelo Venturi <eduardo_venturi1@estudante.sesisenai.org.br>
2026-06-29 20:31:48 -0300

    Criei metodos buscar cadastrar excluir

---
+++ src/data/usuariosData.ts
@@ -0,0 +1,56 @@
+import { conexao } from "@/lib/conexao";
+import { Usuario } from "@/classes/Usuario";
+
+export async function listaUsuario() {
+    const [resultado] = await conexao.query(
+        "SELECT * FROM usuarios",
+    );
+    return resultado;
+}
+export async function buscarUsuarioPorID(id: number) {
+    const { resultado }: any = await conexao.query(
+        "SELECT * FROM usuarios WHERE id = ?",
+        [id]
+
+    );
+    return resultado[0];
+
+}
+
+export async function cadastrarUsuario(usuario: Usuario) {
+    const [resultado]: any = await conexao.query(
+        "INSERT INTO usuario (nome,email,senha,endereco,tipo_usuario)values(?,?,?,?)",
+        [
+
+            usuario.nome,
+            usuario.email,
+            usuario.endereco,
+            usuario.senha
+        ]
+
+    );
+    return resultado.insertId;
+
+}
+export async function editarUsuario (id:number, usuario : Usuario) {
+    const [resultado]: any =await conexao.query(
+        "UPDATE usuario SET nome = ? , email = ?,senha = ? ,endereco = ?,tipo_usuario = ? WHERE id = ?",
+    [
+
+         usuario.nome,
+         usuario.email,
+         usuario.endereco,
+         usuario.senha,
+         id
+    ]
+    
+    );
+    return resultado.affectedRows > 0;
+}
+
+export async function  excluirUsuario (id:number) {
+    const [resultado] :any = await conexao.query(
+        [id]
+    );
+    return resultado.affectedRows > 0 ;
+}
